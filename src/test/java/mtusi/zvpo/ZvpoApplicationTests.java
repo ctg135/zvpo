ac6efae56ed1f96c4fd6f5d29bb70f1ca0aa99f2
ctg135 <salabaevr@yandex.ru>
2025-03-02 13:17:30 +0300

    First commit

---
+++ src/test/java/mtusi/zvpo/ZvpoApplicationTests.java
@@ -0,0 +1,13 @@
+package mtusi.zvpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZvpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
