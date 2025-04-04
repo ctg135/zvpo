f3df71c3a4bd325e862e333e6c9808ebfa3500c4
ctg135 <salabaevr@yandex.ru>
2025-04-04 22:48:28 +0300

    Signature controller with todos

---
+++ src/main/java/mtusi/zvpo/SignatureController.java
@@ -0,0 +1,78 @@
+package mtusi.zvpo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Console;
+import java.io.Writer;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/signature")
+public class SignatureController {
+    private SignatureRepository signatureRepository;
+
+    public SignatureController(SignatureRepository signatureRepository){
+        this.signatureRepository = signatureRepository;
+    }
+
+    /*
+    TODO: Контроллеры
+        0. Полный возврат всех сигнатур GET
+            Возвращает все сигнатуры с нормальным статусом (не DELETED и т.п.)
+        1. Получение диффа POST
+            По параметру since получить все сигнатуры с updated_at позднее
+        2. По GUID
+            По списку GUID вернуть список сигнатур
+        3. Добавление новой сигнатуры
+        4. Удаление сигнатуры
+            Ставит сигнатуре статус DELETED
+        5. Обновление записи
+            1. По новым данным создается новая запись
+            2. На старой записи ставится статус UPDATED
+            3. Измененные поля записываются в таблицу audit
+        6. Доступ к записям по статусу
+            По статусу вернуть список сигнатур
+     */
+
+    @GetMapping
+    @RequestMapping("/get")
+    public ResponseEntity<List<SignatureEntity>> getSignatures(){
+        // Список актуальных сигнатур
+        /*
+        TODO: Вместо объектов сигнатур пустые скобки, похоже не сериализуется
+         */
+        var signatures = signatureRepository.findAll();
+        return ResponseEntity.status(200).body(signatures);
+    }
+
+    @PostMapping
+    @RequestMapping("/get/diff")
+    public ResponseEntity<List<SignatureEntity>> getSignaturesByDiff(
+            @BindParam("since")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            Date since){
+        // Список сигнатур с какой-то даты
+        /*
+        TODO: Возвращает пустой список всегда. Видимо нужно добавить
+         */
+        var signatures = signatureRepository.findByUpdatedAtAfter(since);
+        return ResponseEntity.status(200).body(signatures);
+    }
+
+    @PostMapping
+    @RequestMapping("/get/uuids")
+    public ResponseEntity<List<SignatureEntity>> getSignaturesByUUID(
+            @BindParam("uuids") List<UUID> uuids){
+        // Список сигнатур по списку UUID
+        /*
+        TODO: Падает на этапе обработки входных данных массива
+         */
+        var signatures = signatureRepository.findByIdIn(uuids);
+        return ResponseEntity.status(200).body(signatures);
+    }
+
+}
--- src/main/java/mtusi/zvpo/SignatureRepository.java
+++ src/main/java/mtusi/zvpo/SignatureRepository.java
@@ -4,9 +4,13 @@ package mtusi.zvpo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface SignatureRepository extends JpaRepository<SignatureEntity, UUID> {
-
+    List<SignatureEntity> findByStatus(String status);
+    List<SignatureEntity> findByUpdatedAtAfter(Date since);
+    List<SignatureEntity> findByIdIn(List<UUID> uuids);
 }
